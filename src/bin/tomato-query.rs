d5a484ddc806d5022e20be344d3538158f98ea0a
Richard Yao <2501.owlsome@gmail.com>
2021-01-22 19:39:17 +0800

    refactor clock framework and impl remote control by socket

--- src/bin/tomato-query.rs
+++ src/bin/tomato-query.rs
@@ -1,12 +1,30 @@
+use std::env;
 use std::io::prelude::*;
 use std::os::unix::net::UnixStream;
 
 static UNIX_ADDRESS: &'static str = "/tmp/tomato-notity-socket";
+static USAGE: &'static str = "tomato-query [ --get-info | --ready | --remind ]";
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() > 2 {
+        panic!(USAGE);
+    }
+    let command = match args.get(1) {
+        Some(command) => match command.as_str() {
+            "--get-info" => "GET INFO",
+            "--ready" => "READY",
+            "--remind" => "REMIND",
+            _ => panic!(USAGE),
+        },
+        None => "GET INFO",
+    };
+    let mut command = command.to_string();
+    command.push_str("\n");
+
     let mut stream = UnixStream::connect(UNIX_ADDRESS).unwrap();
     let mut response = String::new();
-    stream.write_all("GET INFO\n".as_bytes()).unwrap();
+    stream.write_all(command.as_bytes()).unwrap();
     // stream.write_all("GET ".as_bytes()).unwrap();
     // std::thread::sleep(std::time::Duration::from_millis(800));
     // stream.write_all("INFO\n".as_bytes()).unwrap();
--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,24 @@
 use std::error::Error;
+use std::fmt::Debug;
 use std::io;
-use std::sync::{Arc, Mutex};
 use std::time::{Duration, Instant};
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::{UnixListener, UnixStream};
 use tokio::process::Command;
 use tokio::signal;
-use tokio::sync::watch;
+use tokio::sync::{mpsc, watch};
 use tokio::task::JoinHandle;
 use tokio::time::{sleep, timeout};
 
 static UNIX_ADDRESS: &'static str = "/tmp/tomato-notity-socket";
-static BUSY_DURATION: u64 = 1;
-static SHORT_BREAK_DURATION: u64 = 1;
-static LONG_BREAK_DURATION: u64 = 1;
-static NOTIFY_REMIND_DURATION: u64 = 1;
+static BUSY_DURATION: u64 = 25;
+static SHORT_BREAK_DURATION: u64 = 5;
+static LONG_BREAK_DURATION: u64 = 15;
+static NOTIFY_REMIND_DURATION: u64 = 2;
 
 #[inline(always)]
 fn min2sec(min: u64) -> u64 {
-    min * 5
+    min * 60
 }
 
 fn dura_sub(lhs: &Duration, rhs: &Duration) -> i32 {
@@ -35,90 +35,141 @@ async fn sleep_min(min: u64) {
     sleep(Duration::from_secs(min2sec(min))).await;
 }
 
+type Quit = watch::Receiver<bool>;
+
+fn prepare_quit_signal() -> (JoinHandle<()>, Quit) {
+    let (tx, rx) = watch::channel(false);
+    let handle = tokio::spawn(async move {
+        signal::ctrl_c().await.unwrap();
+        tx.send(true).unwrap();
+    });
+    (handle, rx)
+}
+
+#[derive(Debug)]
+enum TomatoActions {
+    Ready,
+    Remind,
+}
+
+#[derive(Clone, Debug)]
 enum TomatoStatus {
     Busy,
     ShortBreak,
     LongBreak,
 }
 
+#[derive(Clone, Debug)]
 struct ClockInfo {
     tomato_status: TomatoStatus,
+    next_status: TomatoStatus,
+    need_notify: bool,
     last_checked: Instant,
 }
 
 struct TomatoClock {
-    status: Arc<Mutex<ClockInfo>>,
-    notifier: Notifier,
+    status_output: watch::Sender<ClockInfo>,
+    action_input: mpsc::Receiver<TomatoActions>,
 }
 
 impl TomatoClock {
-    fn new() -> Self {
-        Self {
-            status: Arc::new(Mutex::new(ClockInfo {
-                tomato_status: TomatoStatus::Busy,
-                last_checked: Instant::now(),
-            })),
-            notifier: Notifier::new(),
+    fn new() -> (
+        Self,
+        mpsc::Sender<TomatoActions>,
+        watch::Receiver<ClockInfo>,
+    ) {
+        let status = ClockInfo {
+            tomato_status: TomatoStatus::Busy,
+            next_status: TomatoStatus::ShortBreak,
+            need_notify: false,
+            last_checked: Instant::now(),
+        };
+        let (action_tx, action_rx) = mpsc::channel(8);
+        let (status_tx, status_rx) = watch::channel(status);
+        let tomato_clock = Self {
+            status_output: status_tx,
+            action_input: action_rx,
+        };
+        (tomato_clock, action_tx, status_rx)
+    }
+
+    // [Fix me] bad solution
+    async fn _clear_action_input(&mut self) {
+        let exhaust_receiver = async { while let Some(_) = self.action_input.recv().await {} };
+        let _ = timeout(Duration::from_millis(100), exhaust_receiver).await;
+    }
+
+    async fn _wait_for_ready_action(&mut self) {
+        loop {
+            // action_input.recv return None when other senders were closed
+            let action = self.action_input.recv().await.unwrap();
+            if let TomatoActions::Ready = action {
+                break;
+            }
         }
     }
 
-    fn get_info(&self) -> Arc<Mutex<ClockInfo>> {
-        Arc::clone(&self.status)
-    }
-
-    async fn notify(&self, msg: &str) -> bool {
-        let actions = [("ok", "Ok"), ("remind", "Remind me later")];
-        let selection = self.notifier.notify(msg, &actions).await;
-        match selection {
-            Ok(Some(selection)) if selection == "ok" => true,
-            _ => false,
+    async fn notify(&mut self) {
+        self._clear_action_input().await;
+        loop {
+            let mut notify_status = self.status_output.borrow().clone();
+            notify_status.need_notify = true;
+            self.status_output.send(notify_status).unwrap();
+            tokio::select! {
+                _ = self._wait_for_ready_action() => {break;},
+                _ = sleep_min(NOTIFY_REMIND_DURATION) => {},
+            }
         }
     }
 
-    async fn clock_logic(&self) {
+    async fn clock_logic(&mut self) {
         let mut manual_start = true;
         loop {
             for tomato in 0..4 {
                 if manual_start {
                     manual_start = false;
                 } else {
-                    while !self.notify("start work").await {
-                        sleep_min(NOTIFY_REMIND_DURATION).await;
-                    }
+                    self.notify().await;
                 }
-                {
-                    let mut status = self.status.lock().unwrap();
-                    status.last_checked = Instant::now();
-                    status.tomato_status = TomatoStatus::Busy;
-                }
+                let status = ClockInfo {
+                    tomato_status: TomatoStatus::Busy,
+                    next_status: if tomato != 3 {
+                        TomatoStatus::ShortBreak
+                    } else {
+                        TomatoStatus::LongBreak
+                    },
+                    need_notify: false,
+                    last_checked: Instant::now(),
+                };
+                self.status_output.send(status).unwrap();
                 sleep_min(BUSY_DURATION).await;
 
                 if tomato != 3 {
-                    while !self.notify("short break").await {
-                        sleep_min(NOTIFY_REMIND_DURATION).await;
-                    }
-                    {
-                        let mut status = self.status.lock().unwrap();
-                        status.last_checked = Instant::now();
-                        status.tomato_status = TomatoStatus::ShortBreak;
-                    }
+                    self.notify().await;
+                    let status = ClockInfo {
+                        tomato_status: TomatoStatus::ShortBreak,
+                        next_status: TomatoStatus::Busy,
+                        need_notify: false,
+                        last_checked: Instant::now(),
+                    };
+                    self.status_output.send(status).unwrap();
                     sleep_min(SHORT_BREAK_DURATION).await;
                 } else {
-                    while !self.notify("long break").await {
-                        sleep_min(NOTIFY_REMIND_DURATION).await;
-                    }
-                    {
-                        let mut status = self.status.lock().unwrap();
-                        status.last_checked = Instant::now();
-                        status.tomato_status = TomatoStatus::LongBreak;
-                    }
+                    self.notify().await;
+                    let status = ClockInfo {
+                        tomato_status: TomatoStatus::LongBreak,
+                        next_status: TomatoStatus::Busy,
+                        need_notify: false,
+                        last_checked: Instant::now(),
+                    };
+                    self.status_output.send(status).unwrap();
                     sleep_min(LONG_BREAK_DURATION).await;
                 }
             }
         }
     }
 
-    async fn run(self, mut control: Quit) {
+    async fn run(mut self, mut control: Quit) {
         tokio::select! {
             // the whole clock_logic process can be intercepted
             _ = self.clock_logic() => {},
@@ -141,24 +192,27 @@ impl TomatoClock {
     }
 }
 
-type Quit = watch::Receiver<bool>;
-
-struct Remote {
+struct SocketRemote {
     listener: UnixListener,
-    clock_status: Arc<Mutex<ClockInfo>>,
+    clock_status: watch::Receiver<ClockInfo>,
+    action_sender: mpsc::Sender<TomatoActions>,
 }
 
-impl Remote {
-    fn new(clock_status: Arc<Mutex<ClockInfo>>) -> Result<Self, Box<dyn Error>> {
-        let reporter = Remote {
+impl SocketRemote {
+    fn new(
+        clock_status: watch::Receiver<ClockInfo>,
+        action_sender: mpsc::Sender<TomatoActions>,
+    ) -> Result<Self, Box<dyn Error>> {
+        let reporter = SocketRemote {
             listener: UnixListener::bind(UNIX_ADDRESS)?,
             clock_status,
+            action_sender,
         };
         Ok(reporter)
     }
 
     fn api_get_info(&self) -> String {
-        let clock_status = self.clock_status.lock().unwrap();
+        let clock_status = self.clock_status.borrow();
         let elapsed = clock_status.last_checked.elapsed();
         let due_duration = match clock_status.tomato_status {
             TomatoStatus::Busy => Duration::from_secs(min2sec(BUSY_DURATION)),
@@ -168,9 +222,24 @@ impl Remote {
         dura_sub(&due_duration, &elapsed).to_string()
     }
 
+    async fn api_ready(&self) -> String {
+        self.action_sender.send(TomatoActions::Ready).await.unwrap();
+        "OK".to_string()
+    }
+
+    async fn api_remind(&self) -> String {
+        self.action_sender
+            .send(TomatoActions::Remind)
+            .await
+            .unwrap();
+        "OK".to_string()
+    }
+
     async fn send(&self, stream: &mut UnixStream, command: &str) -> std::io::Result<()> {
         let msg = match command {
             "GET INFO" => self.api_get_info(),
+            "READY" => self.api_ready().await,
+            "REMIND" => self.api_remind().await,
             _ => {
                 return Err(io::Error::new(
                     io::ErrorKind::InvalidData,
@@ -268,25 +337,80 @@ impl Remote {
     }
 }
 
-impl Drop for Remote {
+impl Drop for SocketRemote {
     fn drop(&mut self) {
         std::fs::remove_file(UNIX_ADDRESS).unwrap();
     }
 }
 
-struct Notifier {
+struct NotifierRemote {
     backend: String,
+    clock_status: watch::Receiver<ClockInfo>,
+    action_sender: mpsc::Sender<TomatoActions>,
 }
 
-impl Notifier {
-    fn new() -> Self {
+impl NotifierRemote {
+    fn new(
+        clock_status: watch::Receiver<ClockInfo>,
+        action_sender: mpsc::Sender<TomatoActions>,
+    ) -> Self {
         let backend = "dunstify";
-        Notifier {
+        NotifierRemote {
             backend: backend.to_string(),
+            clock_status,
+            action_sender,
         }
     }
 
-    async fn notify(&self, msg: &str, actions: &[(&str, &str)]) -> Result<Option<String>, ()> {
+    async fn run(mut self, mut control: Quit) {
+        tokio::select! {
+            // the whole clock_logic process can be intercepted
+            _ = self.tomato_notify() => {},
+            res = control.changed() => {
+                match res {
+                    Ok(_) => {
+                        println!("NotifierRemote exit with {:?}", res);
+                        println!("control = {}", *control.borrow());
+                        if *control.borrow() {
+                            return;
+                        }
+                    },
+                    Err(e) => {
+                        eprintln!("NotifierRemote quit control: {}", e);
+                        return;
+                    }
+                }
+            }
+        }
+    }
+
+    async fn tomato_notify(&mut self) {
+        while self.clock_status.changed().await.is_ok() {
+            if !self.clock_status.borrow().need_notify {
+                continue;
+            }
+            let msg = match self.clock_status.borrow().next_status {
+                TomatoStatus::Busy => "Start work",
+                TomatoStatus::ShortBreak => "Shor break",
+                TomatoStatus::LongBreak => "Long break",
+            };
+            let actions = [("ok", "Ok"), ("remind", "Remind me later")];
+            let selection = self._notify(msg, &actions).await;
+            match selection {
+                Ok(Some(selection)) if selection == "ok" => {
+                    self.action_sender.send(TomatoActions::Ready).await.unwrap();
+                }
+                _ => {
+                    self.action_sender
+                        .send(TomatoActions::Remind)
+                        .await
+                        .unwrap();
+                }
+            }
+        }
+    }
+
+    async fn _notify(&self, msg: &str, actions: &[(&str, &str)]) -> Result<Option<String>, ()> {
         let actions: Vec<String> = actions
             .iter()
             .map(|(action, label)| format!("--action={},{}", action, label))
@@ -310,31 +434,27 @@ impl Notifier {
     }
 }
 
-fn prepare_quit_signal() -> (JoinHandle<()>, Quit) {
-    let (tx, rx) = watch::channel(false);
-    let handle = tokio::spawn(async move {
-        signal::ctrl_c().await.unwrap();
-        tx.send(true).unwrap();
-    });
-    (handle, rx)
-}
-
 #[tokio::main]
 async fn main() {
     let (quit_handle, quit_signal) = prepare_quit_signal();
 
-    let clock = TomatoClock::new();
-    let remote = Remote::new(clock.get_info()).unwrap();
+    let (clock, action_sender, clock_status) = TomatoClock::new();
+    let socket_remote = SocketRemote::new(clock_status.clone(), action_sender.clone()).unwrap();
+    let notifier_remote = NotifierRemote::new(clock_status.clone(), action_sender.clone());
 
     let clock_quit_signal = quit_signal.clone();
-    let clock_handle = tokio::spawn(async move {
-        clock.run(clock_quit_signal).await;
-    });
+    let clock_handle = tokio::spawn(async move { clock.run(clock_quit_signal).await });
 
-    let remote_quit_signal = quit_signal.clone();
-    let remote_handle = tokio::spawn(async move { remote.run(remote_quit_signal).await });
+    let socket_remote_quit_signal = quit_signal.clone();
+    let socket_remote_handle =
+        tokio::spawn(async move { socket_remote.run(socket_remote_quit_signal).await });
+
+    let notifier_remote_quit_signal = quit_signal.clone();
+    let notifier_remote_handle =
+        tokio::spawn(async move { notifier_remote.run(notifier_remote_quit_signal).await });
 
     quit_handle.await.unwrap();
     clock_handle.await.unwrap();
-    remote_handle.await.unwrap();
+    socket_remote_handle.await.unwrap();
+    notifier_remote_handle.await.unwrap();
 }
